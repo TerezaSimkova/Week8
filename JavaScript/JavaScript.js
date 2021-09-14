0cc0c157d7c5869b7340cb56c48d56e336214078
tereza.simkova <tereza.simkova@WINAP2AEKQLN12D>
2021-09-14 16:09:15 +0200

    Add Form

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,44 @@
+﻿//Javascript
+
+function demoOnLoad() {
+    alert("Ciao!");
+}
+function demoOnClick() {
+    alert("Hai cliccato sul tasto!")
+}
+
+function onkeydownDemo() {
+    alert("Hai premuto il tasto")
+}
+
+function demoOnChange() {
+    alert("Hai selezionato qualcosa")
+}
+
+function checkSelection() {
+    let checkBox = document.getElementById("avaible");
+    if (checkBox.checked) {
+
+        alert("ogetto disponibile!")
+    }
+    else {
+        alert("ogetto non disponibile!")
+    }
+}
+
+function changeButtonVisibiliti() {
+    let usernameInput = document.getElementsByClassName("username");
+    let button = document.getElementById("submit");
+    if (usernameInput.value != "") {
+        button.style.visibility = "visibile";
+    }
+}
+
+
+function changeButton() {
+    let usernameInput1 = document.getElementsByClassName("username");
+    let button = document.getElementById("submit2");
+    if (usernameInput1.value != "") {
+        button.disabled = false;
+    }
+}
---
+++ ProjectHotel/JavaScript.js
@@ -0,0 +1,14 @@
+﻿//JavaScript
+
+
+function demoOnLoad() {
+    alert("***Benvenuto!***");
+}
+
+function openForm() {
+    document.getElementById("Form").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("Form").style.display = "none";
+}
