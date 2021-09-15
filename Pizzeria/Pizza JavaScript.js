c4a3cb1b1636989ddb487b5c6bb4746373a9e080
tereza.simkova <tereza.simkova@WINAP2AEKQLN12D>
2021-09-15 17:48:45 +0200

    modifica

---
+++ Pizzeria/Pizza JavaScript.js
@@ -0,0 +1,7 @@
+﻿function openNav() {
+    document.getElementById("navigazione").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("navigazione").style.width = "0";
+}
