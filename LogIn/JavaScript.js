6f06b96e93a32963ef8ffcbed4f51e948afeef49
tereza.simkova <tereza.simkova@WINAP2AEKQLN12D>
2021-09-16 11:15:12 +0200

    modifica

---
+++ LogIn/JavaScript.js
@@ -0,0 +1,62 @@
+﻿function CheckedLogUser() {
+    let storedUsername = localStorage.getItem("username"); /*Recupero i dati passandoli la Key */
+    let storedPassword = localStorage.getItem("password");
+
+    if (storedUsername != null && storedPassword != null) {
+
+        hideForm();
+    }
+}
+
+function hideForm() {
+    let form = document.getElementById("loginForm");
+
+    form.style.display = none; //nasconde lo form 
+
+    let logoutButton = document.getElementById("logOutButton");
+
+    logoutButton.style.display = "";
+}
+
+function save() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    //salvo tutto nel local storage
+    localStorage.setItem("Username", username);
+    localStorage.setItem("Password", password);
+}
+
+function change() {
+
+    //se i campiu sono compilati --> login effetuato con successo
+    //recupero elementi tramite input type
+    let inputs = document.querySelectorAll("input[type='text'], input[type='password']");
+    //recupero il tasto di login 
+    let loginButton = document.querySelector("input[type=submit]");
+
+    //Ciclo su inputs per verificare se sono compilati o meno
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == "") //se l'i-esimo campo input ha valore stringa vuota
+        {
+            loginButton.disabled = true; //oppure button.disabled="disabled"; => è disabilitato se il campo non è compilato
+            return; //esce se trova un campo non compilato
+        }
+    }
+    loginButton.disabled = false; //arriva qui solo se entrambi i campi sono compilati
+}
+
+function logOut() {
+
+    //ripulire il localstorage
+    localStorage.clear();
+
+    //recuperare la form e mostrarla di nuovo 
+    let form = document.getElementById("loginForm");
+    form.style.display = "";
+
+    //recuperare e nascondere tasto logout
+    let logoutButton = document.getElementById("logOutButton");
+    logoutButton.style.display = "none";
+
+}
--- Pizzeria/Pizza JavaScript.js
+++ Pizzeria/Pizza JavaScript.js
@@ -5,3 +5,62 @@
 function closeNav() {
     document.getElementById("navigazione").style.width = "0";
 }
+
+//function openForm() {
+//    document.getElementById("form").style.display = "block";
+//}
+//function closeForm() {
+//    document.getElementById("form").style.display = "none";
+
+//}
+
+function calcBill() {
+
+    let checkboxes = document.querySelectorAll("input[type='checkbox']");
+    let priceData = document.querySelectorAll("td#prezzo");
+    let bill = 0.0;
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            bill += parseInt(priceData[i].innerHTML)
+        }
+    }
+    if (bill != 0.0) {
+        localStorage.setItem("conto", bill);
+    }
+    console.log(bill);
+    console.log(priceData);
+    let pageMenu = document.getElementById("changePage");
+    pageMenu.setAttribute("href", "Menu.html");
+ 
+}
+function showCardInput() {
+    let cardInput = document.getElementById("codiceCarta");
+    cardInput.setAttribute("required", "");    //turns required off
+    cardInput.required = false;
+
+    let cashChoice = document.querySelector("input[value='1']");
+    let cardChoice = document.querySelector("input[value='2']");
+
+    if (cardChoice.checked) {
+        cardInput.style.display = "";
+        cardInput.setAttribute("required", "");    //turns required on
+        cardInput.required = true;
+    }
+    else if (cashChoice.checked) {
+        cardInput.style.display = "none"
+    }
+}
+
+function showBill() {
+    let p = document.getElementById("bill");
+    let bill = localStorage.getItem("conto");
+    p.innerHTML = "Stai per pagare: " + bill + "€";
+    localStorage.clear();
+   
+}
+
+function myFunction() {
+     document.getElementById("form").reset();
+
+}
